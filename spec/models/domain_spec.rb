197c56a2859597af1d528e4dabc2c89d2f494919
Leandro Maríngolo <leandronsp@gmail.com>
2016-05-24 15:45:46 +0100

    spec for reset

--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -9,4 +9,26 @@ describe Domain do
     domain = described_class.create url: 'http://mysample.com'
     expect(domain.pages.first.url).to eq('/')
   end
+
+  describe '#reset!' do
+    let(:domain) { described_class.create url: 'http://mysample.com' }
+
+    before do
+      mass_insert = MassInsert.new(domain)
+      mass_insert.insert_pages  [{ url: '/another' }]
+      mass_insert.insert_assets '/another', [{ url: '/asset.css' }]
+    end
+
+    it 'deletes all pages and assets within domain but keeps the root page' do
+      domain.reload
+      expect(domain.assets.count).to eq(1)
+      expect(domain.pages.count).to eq(2)
+
+      domain.reset!
+
+      expect(domain.assets.count).to eq(0)
+      expect(domain.pages.count).to eq(1)
+      expect(domain.pages.first.url).to eq('/')
+    end
+  end
 end
